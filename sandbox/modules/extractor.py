cab3a03d677e03bbb78299e0f5da8957b169c230
Ohmatheus <max.marechal@gmail.com>
2025-02-25 10:39:58 +0100

    Adding public/private attributes in extractor module

--- sandbox/modules/extractor.py
+++ sandbox/modules/extractor.py
@@ -1,6 +1,5 @@
 # Encapsulate all functions that transform pdf -> DataFrame
 
-
 import pandas as pd
 
 from langchain_core.pydantic_v1 import BaseModel, Field
@@ -15,8 +14,11 @@ from typing import Optional, List
 
 from logger import logger
 
+__all__ = ["public_function", "PUBLIC_VAR"]
+
 #---------------------------------------------------------------------------
-class InsuranceExtraction_Rule(BaseModel):
+# In the current form, those attributes will also be the dataframe headers
+class _InsuranceExtraction_Rule(BaseModel):
     '''Different informations extracted from a company insurance's rule & rate files.'''
     # This doc-string is sent to the LLM as the description of the schema Person,
     # and it can help to improve extraction results.
@@ -44,7 +46,7 @@ class InsuranceExtraction_Rule(BaseModel):
     )
 
 #---------------------------------------------------------------------------
-prompt_template = ChatPromptTemplate.from_messages(
+_prompt_template = ChatPromptTemplate.from_messages(
     [
         (
             "system",
@@ -61,14 +63,14 @@ prompt_template = ChatPromptTemplate.from_messages(
 )
 
 #---------------------------------------------------------------------------
-def split_pdf(file_path) -> List[Document]:
+def _split_pdf(file_path) -> List[Document]:
     loader = PyPDFLoader(file_path)
     pages = loader.load_and_split()
     return pages
 
 
 #---------------------------------------------------------------------------
-def chunk_text(docs: List[Document]) -> List[str]:
+def _chunk_text(docs: List[Document]) -> List[str]:
     text_splitter = TokenTextSplitter(
         chunk_size=10000,
         chunk_overlap=20
@@ -81,11 +83,11 @@ def chunk_text(docs: List[Document]) -> List[str]:
 
 
 #---------------------------------------------------------------------------
-def extract(texts: List[str], llm) -> List[str]:
+def _extract(texts: List[str], llm) -> List[str]:
     total = len(texts)
     logger.info(f"Starting extraction on {total} chunks.")
-    structured_llm_rule = llm.with_structured_output(schema=InsuranceExtraction_Rule, include_raw=False)
-    extractor = prompt_template | structured_llm_rule
+    structured_llm_rule = llm.with_structured_output(schema=_InsuranceExtraction_Rule, include_raw=False)
+    extractor = _prompt_template | structured_llm_rule
     try:
         extractions = extractor.batch(
             [{"text": text} for text in texts],
@@ -98,7 +100,7 @@ def extract(texts: List[str], llm) -> List[str]:
 
 
 #---------------------------------------------------------------------------
-def convert_to_df(exts: List[str]) -> pd.DataFrame:
+def _convert_to_df(exts: List[str]) -> pd.DataFrame:
     extractions_dict = [extraction.__dict__ for extraction in exts]
     df = pd.DataFrame(extractions_dict)
     return df
@@ -108,8 +110,8 @@ def convert_to_df(exts: List[str]) -> pd.DataFrame:
 def process_pdf(file_path, llm, **kwargs):
     """Function to extract info from a PDF and return a DataFrame."""
     logger.info(f"Starting load and extraction of {file_path}")
-    pages = split_pdf(file_path)
-    text_processed = chunk_text(pages)
-    extractions = extract(text_processed, llm)
-    df = convert_to_df(extractions)
+    pages = _split_pdf(file_path)
+    text_processed = _chunk_text(pages)
+    extractions = _extract(text_processed, llm)
+    df = _convert_to_df(extractions)
     return df
